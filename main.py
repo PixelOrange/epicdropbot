bec7e33e673eaf43244f7fd29b93a51d1cc81826
Kyle Thomas <kcthomas@gmail.com>
2021-11-08 00:00:48 -0600

    Add init file function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # bot.py
 import os
-import random
+import json
 from discord.ext import commands
 from dotenv import load_dotenv
 
@@ -8,4 +8,23 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
-bot.run(TOKEN)
+
+def initialize():
+  try:
+    with open('user_data.json') as json_file:
+      data = json.load(json_file)
+      if not data:
+        print("no data")
+      else:
+        print(data)
+  except IOError:
+    print("No file found")
+
+
+def start():
+  print("hello")
+  initialize()
+  bot.run(TOKEN)
+
+
+start()
